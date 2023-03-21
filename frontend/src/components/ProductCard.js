9684b8233b7bae6e894ae93dc736e995d99d892a
alexander0354 <alebrekvi@gmail.com>
2023-03-21 15:19:28 +0100

    forelesning 21.03

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,8 @@
+export default function ProductCard ({productInfo}) {
+    return (
+        <article>
+            <h2>{productInfo.product_title}</h2>
+            <p>Pris: {}</p>
+        </article>
+    )
+}
