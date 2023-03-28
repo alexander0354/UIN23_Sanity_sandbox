67247401fd044f14dc2db5b31be2f44eb03f07c7
alexander0354 <alebrekvi@gmail.com>
2023-03-28 13:31:05 +0200

    fikset fra forelesning 24.03

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { fetchAllProducts } from './lib/sanity/productServices';
 import {useEffect, useState} from 'react'
 import Frontpage from './pages/Frontpage';
 import { Route, Routes } from 'react-router-dom';
+import ProductPage from './pages/ProductPage';
+import CategoriesPage from './pages/CategoriesPage';
+import CategoryPage from './pages/CategoryPage';
+
 
 function App() {
 
@@ -21,12 +25,18 @@ function App() {
   }, [])
 
   console.log(prods)
-
   return (
     <Routes>
-    <Route index element={<Frontpage products={prods} />} />
+      <Route index element={<Frontpage products={prods} />} />
+      <Route path=":slug" element={<ProductPage />} />
+      <Route path="kategori">
+        <Route index element={<CategoriesPage />} />
+        <Route path=":category" element={<CategoryPage />} />
+      </Route>
     </Routes>
+
   );
 }
+
 
 export default App;
--- frontend/src/lib/sanity/CategoryServices.js
+++ frontend/src/lib/sanity/CategoryServices.js
@@ -1,16 +1,6 @@
-import {client} from './client'
+import { client } from "./client"
 
-//Service for å hente alle produkter
-export const fetchAllProducts = async () => {
-    const data = await client.fetch(`*[_type == "products"]`)
-    return data
-}
-
-//Service for å hente et bestemt produkt, basert på :slug
-export const fetchProduct = async (slug) => {
-    const data = await client.fetch(`
-    *[_type == "products" && slug.current == $slug]
-    {product_title, price, quantity, "imageURL":product_image.asset->url, "catname": category->category_title}
-    `,{slug})
+export const fetchAllCategories = async () => {
+    const data = await client.fetch(`*[_type == "categories"]`)
     return data
 }
